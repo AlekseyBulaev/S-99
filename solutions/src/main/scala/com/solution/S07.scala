29dcf0b1a516e7efca8fff9266cfbf49d390841d
AlekseyBulaev <alexeybulaev@gmail.com>
2024-02-12 22:58:31 -0500

    S-99: - problem 7 add solution

--- solutions/src/main/scala/com/solution/S07.scala
+++ solutions/src/main/scala/com/solution/S07.scala
@@ -5,12 +5,17 @@ import scala.annotation.tailrec
 //Flatten a nested list structure.
 
 object S07 {
-  def flatten[A](in: List[A]): List[Any] = {
+  def mySolution[A](in: List[A]): List[Any] = {
     def inner(input: List[Any], result: List[Any]): List[Any] = input match {
-      case inp @ List(List(h, _*), _*) => inner(inp.head.asInstanceOf[List[Any]], result) ++ inner(inp.tail, List())
+      case inp @ List(List(_*), _*) => inner(inp.head.asInstanceOf[List[Any]], result) ++ inner(inp.tail, List())
       case Nil => result
       case _ => inner(input.tail, result ++ List(input.head))
     }
     inner(in, List())
+  }
+
+  def flatten(ls: List[Any]): List[Any] = ls flatMap {
+    case ms: List[_] => flatten(ms)
+    case e => List(e)
   }
 }
